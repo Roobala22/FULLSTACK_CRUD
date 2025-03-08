0fa58690544c5172cf872e9633ac1b4620afe4f2
Roobala22 <144305935+Roobala22@users.noreply.github.com>
2025-03-08 13:17:59 +0530

    Update FullstackBackendApplication.java

--- fullstack-backend-master/src/main/java/com/CODE/fullstackbackend/FullstackBackendApplication.java
+++ fullstack-backend-master/src/main/java/com/CODE/fullstackbackend/FullstackBackendApplication.java
@@ -1,4 +1,4 @@
-package com.codewitharjun.fullstackbackend;
+package com.code.fullstackbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
